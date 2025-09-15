3796754c78ddd3ba080111dc5b6e412c5875376c
Juan Fernández Ceacero <jfc00031@red.ujaen.es>
2025-09-15 18:30:02 +0200

    Leer datos y pasarlo a la matriz implementado

--- src/main/java/metaheuristicas/app/Main.java
+++ src/main/java/metaheuristicas/app/Main.java
@@ -1,17 +1,83 @@
 package metaheuristicas.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void leerMatriz(int [][] matriz){
+
+        for(int[] fila :  matriz){
+
+            System.out.println(Arrays.toString(fila));
+
         }
+        System.out.println();
+
+    }
+
+    public static int[][][] leerDatosMatriz(String rutaFichero) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(rutaFichero));
+        int tam = Integer.parseInt(br.readLine());
+        br.readLine();
+
+        int [][] F = new int[tam][tam];
+        int [][] D = new int[tam][tam];
+        for (int i = 0; i < tam; i++) {
+
+            String[] datos = br.readLine().trim().split("\\s+");
+            for (int j = 0; j < tam; j++) {
+
+                F[i][j] = Integer.parseInt(datos[j]);
+
+            }
+
+        }
+
+        br.readLine();
+
+        for (int i = 0; i < tam; i++) {
+
+            String[] datos = br.readLine().trim().split("\\s+");
+            for (int j = 0; j < tam; j++) {
+
+                D[i][j] = Integer.parseInt(datos[j]);
+
+            }
+
+        }
+
+        br.close();
+        return new int[][][]{F,D};
+
+    }
+
+    public static void main(String[] args) {
+
+        try{
+
+            int[][][] matrices = leerDatosMatriz("src/main/resources/_ejemplo.txt");
+            int[][] F = matrices[0];
+            int[][] D = matrices[1];
+
+            System.out.println("Matriz de flujo (F):");
+            leerMatriz(F);
+            System.out.println("Matriz de distancias (D):");
+            leerMatriz(D);
+
+            //TODO Implementar greedy y después greedy aleatorio
+            //TODO Crearse cada uno una rama a partir de develop
+
+        }catch(Exception ex){
+
+            System.err.println("Ha ocurrido un error: " +ex.getMessage());
+
+        }
+
     }
 }
